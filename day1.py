a581162992ae85107f881e5a22a50354978440ec
Per Lovén <loven.per@gmail.com>
2023-12-01 07:26:11 +0100

    Solve 1-1

--- day1.py
+++ day1.py
@@ -1,10 +1,34 @@
 import sys
 
 lines = open(sys.argv[1]).read().split('\n')
-for line in lines:
-    print(line)
 
-ans1 = 0
+
+def part1():
+    calibration_values = []
+    for line in lines:
+        first = find_first_digit(line)
+        last = find_last_digit(line)
+        calibration_values.append(str(first) + str(last))
+
+    ans1 = 0
+    for calVal in calibration_values:
+        ans1 += int(calVal)
+    return ans1
+
+
+def find_first_digit(line):
+    for char in line:
+        if char.isnumeric():
+            return int(char)
+
+
+def find_last_digit(line):
+    for char in reversed(line):
+        if char.isnumeric():
+            return int(char)
+
+
+ans1 = part1()
 ans2 = 0
 print(f'Answer Part 1: {ans1}')
 print(f'Answer Part 2: {ans2}')
